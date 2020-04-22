7b973cb8907d2ec31b27f86265030563e71d68ba
Lulixue <jackreagan@163.com>
2020-04-22 15:11:34 +0800

    添加外轮廓

--- opencv/test.py
+++ opencv/test.py
@@ -1,6 +1,22 @@
 
+import sys
 import numpy as np
 import cv2
+
+
+class InnerContour:
+    topLeft = (sys.maxsize, sys.maxsize)
+    bottomLeft = (0, sys.maxsize)
+    topRight = (sys.maxsize, 0)
+    bottomRight = (0, 0)
+
+    def calculateContour(self, contours):
+        for contour in contours:
+            x = contour.x
+            y = contour.y
+            if x < topLeft.x:
+                pass
+        pass
 
 # img = cv2.imread("text.png")
 img = cv2.imread("df_bu.jpg")
@@ -18,18 +34,36 @@ cnt = contours[0]
 M = cv2.moments(cnt)
 # print(M)
 
-minArea = 20.0
+minArea = 100.0
 drawContours = []
+allContours = []
 for contour in contours:
     area = cv2.contourArea(contour)
-    # print("area: ", area)
+    # print("area: ", area) 
     if area >= minArea:
+        print("area: ", area)
         drawContours.append(contour)
+        for cnt in contour:
+            allContours.append(cnt.tolist())
+        print("contour: ", contour)
         # cv2.drawContours(img, [contour], 0, (0, 0,255), 3)
 
+# print(allContours)
+allContours = np.array(allContours)
+# define main island contour approx. and hull
+perimeter = cv2.arcLength(allContours,True)
+epsilon = 0.01*cv2.arcLength(allContours,True)
+approx = cv2.approxPolyDP(allContours,epsilon,True)
+hull = cv2.convexHull(allContours)
 
+
+# cv2.drawContours(img, drawContours, -1, (0, 0,255), 3)
+# cv2.drawContours(img, [allContours], 0, (0, 0,255), 3)
+
+# cv2.drawContours(img, [approx], -1, (0, 0, 255), 3)
+cv2.drawContours(img, [hull], -1, (0, 0, 255), 3)
 
 cv2.imshow("image", img)
-cv2.imshow("image gray", imgray)
+# cv2.imshow("image gray", imgray)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
