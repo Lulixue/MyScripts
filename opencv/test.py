849c715bb83679457edfc28986b943b5b73c42bd
Lulixue <jackreagan@163.com>
2020-04-22 15:50:24 +0800

    添加重心

--- opencv/test.py
+++ opencv/test.py
@@ -33,10 +33,12 @@ def contourIsOnEdge(contours):
 img = cv2.imread("lq_sui.jpg")
 imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# Creating kernel 
-kernel = np.ones((1, 1), np.uint8) 
-# Using cv2.erode() method  
-imgray = cv2.erode(imgray, kernel) 
+# cv2.imshow("before erode", imgray)
+# # Creating kernel 
+# kernel = np.ones((1, 1), np.uint8) 
+# # Using cv2.erode() method  
+# imgray = cv2.erode(imgray, kernel) 
+# cv2.imshow("after erode", imgray)
 
 size = 10
 imgray = cv2.bilateralFilter(imgray, size, size * 2, size / 2)
@@ -50,9 +52,6 @@ print("len: ", len(contours))
 # print(hierarchy)
 
 
-cnt = contours[0]
-M = cv2.moments(cnt)
-# print(M)
 
 minArea = 100.0
 drawContours = []
@@ -61,7 +60,7 @@ for contour in contours:
     area = cv2.contourArea(contour)
     # print("area: ", area) 
     if area >= minArea:
-        print("area: ", area)
+        # print("area: ", area)
         drawContours.append(contour)
         if contourIsOnEdge(contour):
             continue
@@ -79,12 +78,24 @@ epsilon = 0.01*cv2.arcLength(allContours,True)
 approx = cv2.approxPolyDP(allContours,epsilon,True)
 hull = cv2.convexHull(allContours)
 
-
+print("hull: ", hull)
 cv2.drawContours(img, drawContours, -1, (0, 0,255), 3)
 # cv2.drawContours(img, [allContours], 0, (0, 0,255), 3)
 
 # cv2.drawContours(img, [approx], -1, (0, 0, 255), 3)
-cv2.drawContours(img, [hull], -1, (0, 0, 255), 3)
+cv2.drawContours(img, [hull], -1, (255, 255, 255), 3)
+
+ 
+M = cv2.moments(hull) 
+
+# calculate x,y coordinate of center
+cX = int(M["m10"] / M["m00"])
+cY = int(M["m01"] / M["m00"])
+
+# put text and highlight the center
+cv2.circle(img, (cX, cY), 5, (255, 255, 255), -1)
+cv2.putText(img, "centroid", (cX - 25, cY - 25),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+
 
 cv2.imshow("image", img)
 cv2.imshow("image gray", imgray)
