313421c5e0f0816681547f6d8b24dc86b00ff18b
Lulixue <jackreagan@163.com>
2020-04-24 14:16:53 +0800

    添加图片合成

--- opencv/stroke.py
+++ opencv/stroke.py
@@ -40,12 +40,36 @@ def contourIsOnEdge(contours):
     return False
 
 
+def resizeImage(img, dW, dH):
+    h, w = img.shape[:2]
+    deltaW = int(abs(w - dW)/2)
+    deltaH = int(abs(h - dH)/2)
+    return img[deltaH:(deltaH+dH), deltaW:(deltaW+dW)]
+
+
 ### load image and grayscale it
 # img = cv2.imread("text.png")
 img = cv2.imread("cz_bu.jpg")
+img2 = cv2.imread("df_bu.jpg")
 imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+h1, w1 = img.shape[:2]
+h2, w2 = img2.shape[:2]
 
+dW = min(w1, w2)
+dH = min(h1, h2)
+
+img1 = resizeImage(img, dW, dH)
+img3 = resizeImage(img2, dW , dH)
+
+print("img1: ", img1.shape)
+print("img3: ", img3.shape)
+dst = cv2.addWeighted(img1,0.75,img3,0.25,0)
+cv2.imshow('dst',dst)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+exit()
 ### erode image
 # cv2.imshow("before erode", imgray)
 # # Creating kernel 
@@ -115,7 +139,6 @@ cv2.drawContours(img, drawContours, -1, (0, 0,255), 3)
 
 # cv2.drawContours(img, [approx], -1, (0, 0, 255), 3)
 cv2.drawContours(img, [hull], -1, (255, 255, 255), 3)
-
 
 
 ### crop contour
