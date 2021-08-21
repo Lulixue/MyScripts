a68a41d9468a305adba46fe77a7a913d1f39f62f
Lulixue <jackreagan@163.com>
2021-08-21 20:50:06 +0800

    Add files via upload

---
+++ clean_as.sh
@@ -0,0 +1,10 @@
+rm -Rvf /Applications/Android\ Studio.app
+rm -Rvf ~/Library/Preferences/AndroidStudio*
+rm ~/Library/Preferences/com.google.android.studio.plist
+rm -Rvf ~/Library/Application\ Support/Google/AndroidStudio*
+rm -Rvf ~/Library/Logs/AndroidStudio*
+rm -Rvf ~/Library/Caches/AndroidStudio*
+rm -Rvf ~/Library/Caches/Google/AndroidStudio*
+rm -Rvf ~/.gradle
+rm -Rvf ~/.android
+rm -Rvf ~/Library/Android*
